168fa444ff6a62c1e1090f9cc51612dbff90589b
Agnieszka Kramarek <agnieszka.kramarek@imapp.pl>
2015-12-16 10:03:10 +0100

    llvm optimization

--- kaleidoscope_evaluator.py
+++ kaleidoscope_evaluator.py
@@ -17,14 +17,25 @@ class Evaluator:
 
         self.target = llvm.Target.from_default_triple()
 
-    def evaluate(self, string):
+    def evaluate(self, string, optimize=True):
         ast = self.parser.parse(string)
         self.generator.generate_llvm(ast)
         if not (isinstance(ast, FunctionNode) and ast.is_anonymous()):
             return None
 
+        print("-------------- Generated -------------------")
         print(str(self.generator.module))
         llvm_mod = llvm.parse_assembly(str(self.generator.module))
+
+        if optimize:
+            pmb = llvm.create_pass_manager_builder()
+            pmb.opt_level = 2
+            pm = llvm.create_module_pass_manager()
+            pmb.populate(pm)
+            pm.run(llvm_mod)
+            print("-------------- Optimized -------------------")
+            print(str(llvm_mod))
+
         target_machine = self.target.create_target_machine()
         with llvm.create_mcjit_compiler(llvm_mod, target_machine) as ee:
             ee.finalize_object()
