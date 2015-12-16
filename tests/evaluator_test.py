41b483d163ea7f029d6b0db29d5f7be82db5f562
Agnieszka Kramarek <agnieszka.kramarek@imapp.pl>
2015-12-16 09:40:49 +0100

    evaluator tests

--- tests/evaluator_test.py
+++ tests/evaluator_test.py
@@ -4,8 +4,25 @@ from kaleidoscope_evaluator import Evaluator
 
 
 class TestEvaluator(unittest.TestCase):
+    def setUp(self):
+        self.evaluator = Evaluator()
+
     def test_basic(self):
-        e = Evaluator()
-        self.assertEqual(e.evaluate("3"), 3.0)
-        self.assertEqual(e.evaluate("3+3*4"), 15.0)
-        self.assertEqual(e.evaluate("3*4-2+5/5"), 11.0)
+        self.assertEqual(self.evaluator.evaluate("3"), 3.0)
+        self.assertEqual(self.evaluator.evaluate("3+3*4"), 15.0)
+        self.assertEqual(self.evaluator.evaluate("3*4-2+5/5"), 11.0)
+
+    def test_def(self):
+        self.assertIsNone(self.evaluator.evaluate("def adder(x y) x+y"))
+        self.assertEqual(self.evaluator.evaluate("adder(5, 4)"), 9.0)
+        self.assertEqual(self.evaluator.evaluate("adder(5, 4) + adder(3, 2)"), 14.0)
+
+    def test_extern(self):
+        self.assertIsNone(self.evaluator.evaluate('extern ceil(x)'))
+        self.assertEqual(self.evaluator.evaluate('ceil(19.5)'), 20.0)
+
+    def test_def_extern(self):
+        self.assertIsNone(self.evaluator.evaluate('extern ceil(x)'))
+        self.assertIsNone(self.evaluator.evaluate('extern floor(x)'))
+        self.assertIsNone(self.evaluator.evaluate("def eadder(x y) ceil(x) + floor(y)"))
+        self.assertEqual(self.evaluator.evaluate("eadder(5.5, 4.2)"), 10.0)
